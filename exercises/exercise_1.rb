d010f0bbf2e6f289db43f4f85ecd6cf8c9747a9e
Taylor Noj <taylornoj@gmail.com>
2022-01-04 03:22:25 +0000

    completed first exercise and viewed table in psql to confirm

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,6 +5,8 @@ puts "----------"
 
 # Your code goes below here ...
 
-Store.create
-Store.create
-Store.create
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true)
+
+puts Store.count
