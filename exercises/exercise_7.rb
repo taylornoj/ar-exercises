d27f153296a406ce644c8186a614d37a8991c740
Taylor Noj <taylornoj@gmail.com>
2022-01-04 16:00:18 +0000

    add validation for both models to enforce rules then show error messages when attempting to add a store

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name:"
+@store = gets.chomp.to_s
+
+new_store = Store.create(name: @store)
+
+# def must_carry_men_or_women_apparel
+#   if !(mens_apparel || womens_apparel)
+#   errors.add(:name, message: "Must carry at least mens or womens apparel")
+#   end
+# end
+
+new_store.errors.full_messages.each {|i| puts i}
