7e0f8ab84ec6f6d5c51ccc86ce54fabfa8b09400
Taylor Noj <taylornoj@gmail.com>
2022-01-04 05:01:39 +0000

    relate the two tables to each other and add employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jessica", last_name: "Smith", hourly_rate: 55)
+@store1.employees.create(first_name: "Deb", last_name: "Burrel", hourly_rate: 65)
+
+@store2.employees.create(first_name: "David", last_name: "Rose", hourly_rate: 67)
+@store2.employees.create(first_name: "John", last_name: "Martins", hourly_rate: 52)
+@store2.employees.create(first_name: "Paul", last_name: "Gladwell",  hourly_rate: 60)
+
