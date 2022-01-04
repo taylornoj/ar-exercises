e2c4619d6e5d9dd6fd5779c488ce148e6e9390e4
Taylor Noj <taylornoj@gmail.com>
2022-01-04 04:43:16 +0000

    calculate total revenue, average revenue and num of stores over 1M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# output total revenue for entire company (all stores)
+puts Store.sum(:annual_revenue)
+
+# output average annual revenue for all stores
+puts Store.average(:annual_revenue)
+
+# output the number of stores generating $1M +
+puts Store.where("annual_revenue > ?", 1000000).count()
